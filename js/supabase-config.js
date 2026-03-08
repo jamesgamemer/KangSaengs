c62891cea2d53348feb8ec7b086b27dafd923bb1
jamesgamemer <jamessarutj@gmail.com>
2026-03-08 22:28:35 +0700

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -8,8 +8,8 @@
    4. Replace the values below
    ============================================================ */
 
-var SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
-var SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY";
+var SUPABASE_URL = "https://gzlkdxigiejwwxewyikq.supabase.co";
+var SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6bGtkeGlnaWVqd3d4ZXd5aWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NjU2NzMsImV4cCI6MjA4ODU0MTY3M30.62lXidTteew5WB2r3kmm6zqBVHB_6AHLz-vWwj3rRrs";
 
 // Check if configured
 if (SUPABASE_URL.includes("YOUR_PROJECT_ID")) {
